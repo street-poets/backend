e36c543cb8ba7dd609bff192e980fbebc683289b
jungwonkim <jungwonkim@jungwonkimui-MacBookAir.local>
2021-02-21 15:43:59 +0900

    프로젝트 셋업, 유저 테이블 생성

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import {TypeOrmModule} from '@nestjs/typeorm'
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'your-password',
+    database: 'geulhouse',
+    autoLoadEntities: true,
+    synchronize: true,
+  }),
+    UserModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
